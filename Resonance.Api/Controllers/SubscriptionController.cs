537a8a87bef9686aff585b36a28d8b1bdc5b0dd3
robertka <robertka@ROBERTKA01>
2016-09-24 10:29:01 +0200

    Initial version of api

---
+++ Resonance.Api/Controllers/SubscriptionController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Resonance.Repo;
+using Resonance.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Resonance.Api.Controllers
+{
+    [Route("subscriptions")]
+    public class SubscriptionController : Controller
+    {
+        private IEventConsumer _consumer;
+
+        public SubscriptionController(IEventConsumer consumer)
+        {
+            _consumer = consumer;
+        }
+
+        [HttpGet]
+        public IEnumerable<Subscription> Get()
+        {
+            return _consumer.GetSubscriptions();
+        }
+
+        // GET api/values/5
+        [HttpGet("{name}")]
+        public IActionResult Get(string name)
+        {
+            var sub = _consumer.GetSubscriptionByName(name);
+            if (sub != null)
+                return Ok(sub);
+            else
+                return NotFound();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Subscription sub)
+        {
+            if (sub != null && TryValidateModel(sub))
+            {
+                try
+                {
+                    return Ok(_consumer.AddOrUpdateSubscription(sub));
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+            else
+                return BadRequest(ModelState);
+        }
+
+        [HttpPut("{name}")]
+        public IActionResult Put(string name, [FromBody]Subscription sub)
+        {
+            if (sub != null && TryValidateModel(sub))
+            {
+                var existingSub = _consumer.GetSubscriptionByName(name);
+                if (existingSub == null)
+                    return NotFound($"No subscription found with name {name}");
+                if (existingSub.Id != sub.Id)
+                    return BadRequest("Id of subscription cannot be modified");
+
+                try
+                {
+                    return Ok(_consumer.AddOrUpdateSubscription(sub));
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+            else
+                return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{name}")]
+        public IActionResult Delete(string name)
+        {
+            if (String.IsNullOrWhiteSpace(name))
+                return BadRequest("Subscription name not provided");
+
+            var existingSub = _consumer.GetSubscriptionByName(name);
+            if (existingSub == null)
+                return NotFound($"No subscription found with name {name}");
+            else
+            {
+                _consumer.DeleteSubscription(existingSub.Id);
+                return Ok();
+            }
+        }
+
+        [HttpGet("{name}/consume")]
+        public IActionResult Consume(string name, int? visibilityTimeout)
+        {
+            try
+            {
+                var ce = _consumer.ConsumeNext(name, visibilityTimeout.GetValueOrDefault(120), 1).FirstOrDefault();
+                if (ce == null)
+                    return NotFound();
+                else
+                    return Ok(ce);
+            }
+            catch (ArgumentException)
+            {
+                return BadRequest($"Subscription with name {name} not found");
+            }
+        }
+    }
+}
---
+++ Resonance.Api/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Resonance.Repo;
+using Resonance.Repo.Database;
+
+namespace Resonance.Api
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add IConfiguration dependency (reason: allows access to config from any injected component)
+            services.AddSingleton<IConfiguration>(Configuration);
+
+            // Configure IEventingRepoFactory dependency (reason: the repo that must be used in this app)
+            // To use MSSQLServer:
+            var connectionString = Configuration.GetConnectionString("Resonance.MsSql");
+            services.AddTransient<IEventingRepoFactory>((p) =>
+            {
+                return new MsSqlEventingRepoFactory(connectionString);
+            });
+            // To use MySQL:
+            //var connectionString = config.GetConnectionString("Resonance.MySql");
+            //serviceCollection.AddTransient<IEventingRepoFactory>((p) =>
+            //{
+            //    return new MySqlEventingRepoFactory(connectionString);
+            //});
+
+            // Configure EventPublisher
+            services.AddTransient<IEventPublisher, EventPublisher>();
+
+            // Configure EventConsumer
+            services.AddTransient<IEventConsumer, EventConsumer>();
+            
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
