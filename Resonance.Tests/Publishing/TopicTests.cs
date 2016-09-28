194df04e2f3ab54b781c0a2afa52e6a71349e3c6
robertka <robertka@ROBERTKA01>
2016-09-28 12:45:02 +0200

    TopicTests added.

--- Resonance.Tests/Publishing/TopicTests.cs
+++ Resonance.Tests/Publishing/TopicTests.cs
@@ -11,36 +11,125 @@ namespace Resonance.Tests.Publishing
     [Collection("EventingRepo")]
     public class TopicTests
     {
-        IEventingRepoFactory _repoFactory;
+        private readonly IEventPublisher _publisher;
 
         public TopicTests(EventingRepoFactoryFixture fixture)
         {
-            _repoFactory = fixture.RepoFactory;
+            _publisher = new EventPublisher(fixture.RepoFactory);
         }
 
         [Fact]
         public void AddTopic()
         {
+            // Arrange
             var topicName = "Publishing.AddTopic";
             var topicNotes = "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";
 
-            using (var repo = _repoFactory.CreateRepo())
-            {
-                var returnedTopic = repo.AddOrUpdateTopic(new Topic { Name = topicName, Notes = topicNotes });
+            // Act
+            var returnedTopic = _publisher.AddOrUpdateTopic(new Topic { Name = topicName, Notes = topicNotes });
 
-                // Checked returned topic
-                Assert.Equal(topicName, returnedTopic.Name);
-                Assert.Equal(topicNotes, returnedTopic.Notes);
-                Assert.True(returnedTopic.Id.HasValue);
-                Assert.True(returnedTopic.Id.Value > 0);
+            // Assert
+            Assert.Equal(topicName, returnedTopic.Name);
+            Assert.Equal(topicNotes, returnedTopic.Notes);
+            Assert.True(returnedTopic.Id.HasValue);
+            Assert.True(returnedTopic.Id.Value > 0);
+        }
 
-                var loadedTopic = repo.GetTopicByName(topicName);
-                Assert.NotNull(loadedTopic);
-                Assert.Equal(topicName, loadedTopic.Name);
-                Assert.Equal(topicNotes, loadedTopic.Notes);
-                Assert.True(loadedTopic.Id.HasValue);
-                Assert.True(loadedTopic.Id.Value > 0);
-            }
+        [Fact]
+        public void UpdateTopic()
+        {
+            // Arrange
+            var topicName = "Publishing.UpdateTopic";
+            var topicNotes = "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";
+
+            // Act
+            var addedTopic = _publisher.AddOrUpdateTopic(new Topic { Name = topicName, Notes = topicNotes });
+            var topicToBeUpdated = new Topic { Id = addedTopic.Id, Name = addedTopic.Name + "_updated", Notes = "updated_" + addedTopic.Notes };
+            var updatedTopic = _publisher.AddOrUpdateTopic(topicToBeUpdated);
+
+            // Assert
+            Assert.Equal(updatedTopic.Id.Value, addedTopic.Id.Value); // Use id from addedTopic, to make sure the id was not modified by the EventPublisher itself
+            Assert.Equal(updatedTopic.Name, topicToBeUpdated.Name);
+            Assert.Equal(updatedTopic.Notes, topicToBeUpdated.Notes);
+        }
+
+        [Fact]
+        public void DeleteTopic()
+        {
+            // Arrange
+            var topicName = "Publishing.DeleteTopic";
+            var topicNotes = "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";
+
+            // Act
+            var addedTopic = _publisher.AddOrUpdateTopic(new Topic { Name = topicName, Notes = topicNotes });
+            _publisher.DeleteTopic(addedTopic.Id.Value, true); // No subscriptions to delete, must add to future test
+
+            // Assert
+            Assert.Null(_publisher.GetTopic(addedTopic.Id.Value));
+        }
+
+        [Fact]
+        public void GetTopicById()
+        {
+            // Arrange
+            var topicName = "Publishing.GetTopicById";
+            _publisher.AddOrUpdateTopic(new Topic { Name = Guid.NewGuid().ToString() }); // Add another to make sure it actually finds it
+            var topicId = _publisher.AddOrUpdateTopic(new Topic { Name = topicName }).Id.Value;
+
+            // Act
+            var topic = _publisher.GetTopic(topicId);
+
+            // Assert
+            Assert.NotNull(topic);
+            Assert.Equal(topicId, topic.Id.Value);
+        }
+
+        [Fact]
+        public void GetTopicByName()
+        {
+            // Arrange
+            var topicName = "Publishing.GetTopicByName";
+            _publisher.AddOrUpdateTopic(new Topic { Name = topicName + "!" }); // Add look-a-likes
+            _publisher.AddOrUpdateTopic(new Topic { Name = "!" + topicName });
+            var topicToBeFound = _publisher.AddOrUpdateTopic(new Topic { Name = topicName });
+
+            // Act
+            var topic = _publisher.GetTopicByName(topicName);
+
+            // Assert
+            Assert.NotNull(topic);
+            Assert.Equal(topicToBeFound.Id.Value, topic.Id.Value);
+            Assert.Equal(topicToBeFound.Name, topic.Name);
+            Assert.Equal(topicToBeFound.Notes, topic.Notes);
+        }
+
+        [Fact]
+        public void GetTopics()
+        {
+            // Arrange
+            var topicName = "Publishing.GetTopics";
+            _publisher.AddOrUpdateTopic(new Topic { Name = Guid.NewGuid().ToString() });
+            _publisher.AddOrUpdateTopic(new Topic { Name = topicName + "1" });
+            _publisher.AddOrUpdateTopic(new Topic { Name = topicName + "2" });
+            _publisher.AddOrUpdateTopic(new Topic { Name = topicName + "3" });
+
+            // Act
+            var topics = _publisher.GetTopics();
+
+            // Assert
+            Assert.NotNull(topics);
+            Assert.True(topics.Count() >= 4); // Maybe more, which were added by other tests
+            Assert.True(topics.Any((t) => t.Name == topicName + "1"));
+            Assert.True(topics.Any((t) => t.Name == topicName + "2"));
+            Assert.True(topics.Any((t) => t.Name == topicName + "3"));
+
+            // Act
+            topics = _publisher.GetTopics(topicName);
+            Assert.NotNull(topics);
+            Assert.True(topics.Count() == 3);
+            Assert.True(topics.Any((t) => t.Name == topicName + "1"));
+            Assert.True(topics.Any((t) => t.Name == topicName + "2"));
+            Assert.True(topics.Any((t) => t.Name == topicName + "3"));
         }
     }
 }
