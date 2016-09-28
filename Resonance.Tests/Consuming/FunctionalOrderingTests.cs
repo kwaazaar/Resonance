e84dc422c2aa5c7cef04b20e4069918c48441632
robertka <robertka@ROBERTKA01>
2016-09-28 17:02:55 +0200

    FunctionalOrdering tests added

---
+++ Resonance.Tests/Consuming/FunctionalOrderingTests.cs
@@ -0,0 +1,76 @@
+﻿using Resonance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Resonance.Tests.Consuming
+{
+    [Collection("EventingRepo")]
+    public class FunctionalOrderingTests
+    {
+        private readonly IEventPublisher _publisher;
+        private readonly IEventConsumer _consumer;
+
+        public FunctionalOrderingTests(EventingRepoFactoryFixture fixture)
+        {
+            _publisher = new EventPublisher(fixture.RepoFactory);
+            _consumer = new EventConsumer(fixture.RepoFactory);
+        }
+
+        [Fact]
+        public void SerialDelivery()
+        {
+            // Arrange
+            var topicName = "FunctionalOrderingTests.SerialDelivery";
+            var subName = topicName + "_Sub1"; // Substring to prevent too long sub-names
+            var topic = _publisher.AddOrUpdateTopic(new Topic { Name = topicName });
+            var sub1 = _consumer.AddOrUpdateSubscription(new Subscription
+            {
+                Name = subName, Ordered = true, MaxDeliveries = 2,
+                TopicSubscriptions = new List<TopicSubscription> { new TopicSubscription { TopicId = topic.Id.Value, Enabled = true } },
+            });
+
+            var publishedDateUtcBaseLine = DateTime.UtcNow.AddSeconds(-60); // Explicitly setting publicationdates to make sure none are the same!
+            _publisher.Publish(topicName, payload: "1", functionalKey: "f1", publicationDateUtc: publishedDateUtcBaseLine.AddSeconds(1));
+            _publisher.Publish(topicName, payload: "2", functionalKey: "f1", publicationDateUtc: publishedDateUtcBaseLine.AddSeconds(2));
+            _publisher.Publish(topicName, payload: "3", functionalKey: "f2", publicationDateUtc: publishedDateUtcBaseLine.AddSeconds(3));
+            _publisher.Publish(topicName, payload: "4", functionalKey: "f1", publicationDateUtc: publishedDateUtcBaseLine.AddSeconds(4));
+            _publisher.Publish(topicName, payload: "5", functionalKey: "f2", publicationDateUtc: publishedDateUtcBaseLine.AddSeconds(5));
+
+            var visibilityTimeout = 5;
+            var p1 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault(); // p1 stands for payload "1"
+            var p3 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            var p2 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            Assert.Equal("1", p1.Payload);
+            Assert.Equal("3", p3.Payload);
+            Assert.Null(p2); // p(ayload) 2 has same functional key as p1 and should not be delivered yet
+
+            _consumer.MarkConsumed(p1.Id, p1.DeliveryKey); // p1 should be gone, so p2 can be delivered
+            p2 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            var p4 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            Assert.Equal("2", p2.Payload);
+            Assert.Null(p4); // Again: same functional key as p2, so should not be delivered (and p3 is still locked)
+            Thread.Sleep(TimeSpan.FromSeconds(visibilityTimeout + 1)); // Wait until visibilitytimeout of all items has expired
+
+            p2 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            p3 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            p4 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            Assert.Equal("2", p2.Payload); // p2 should be redelivered, since it had expired
+            Assert.Equal("3", p3.Payload); // p3 should be redelivered, since it had expired
+            Assert.Null(p4); // p4 still has same functional key as p2, so should not be delivered
+
+            _consumer.MarkFailed(p2.Id, p2.DeliveryKey, Reason.Other("test"));
+            p4 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            Assert.Equal("4", p4.Payload);
+
+            Thread.Sleep(TimeSpan.FromSeconds(visibilityTimeout + 1)); // Wait until visibilitytimeout of all items has expired
+            p4 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            var p5 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            Assert.Equal("4", p4.Payload); // p4 should be redeliverd (it was only delivered once)
+            Assert.Equal("5", p5.Payload); // p3 has reached maxDeliveries, so p5 should now be delivered
+        }
+    }
+}
---
+++ Resonance.Tests/Consuming/StandardOrderingTests.cs
@@ -0,0 +1,89 @@
+﻿using Resonance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Resonance.Tests.Consuming
+{
+    [Collection("EventingRepo")]
+    public class StandardOrderingTests
+    {
+        private readonly IEventPublisher _publisher;
+        private readonly IEventConsumer _consumer;
+
+        public StandardOrderingTests(EventingRepoFactoryFixture fixture)
+        {
+            _publisher = new EventPublisher(fixture.RepoFactory);
+            _consumer = new EventConsumer(fixture.RepoFactory);
+        }
+
+        [Fact]
+        public void PublicationDate_Default()
+        {
+            // Arrange
+            var topicName = "StandardOrderingTests.PublicationDate_Default";
+            var subName = topicName + "_Sub1";
+            var topic = _publisher.AddOrUpdateTopic(new Topic { Name = topicName });
+            var sub1 = _consumer.AddOrUpdateSubscription(new Subscription
+            {
+                Name = subName, // When ordered not set, delivery should still be ordered on publicationdateutc
+                TopicSubscriptions = new List<TopicSubscription> { new TopicSubscription { TopicId = topic.Id.Value, Enabled = true } },
+            });
+
+            _publisher.Publish(topicName, payload: "1");
+            _publisher.Publish(topicName, payload: "2");
+            _publisher.Publish(topicName, payload: "3");
+
+            var visibilityTimeout = 2;
+            var ce1 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            var ce2 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            var ce3 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            Assert.Equal("1", ce1.Payload);
+            Assert.Equal("2", ce2.Payload);
+            Assert.Equal("3", ce3.Payload);
+            _consumer.MarkConsumed(ce2.Id, ce2.DeliveryKey); // ce2 should be gone
+
+            Thread.Sleep(TimeSpan.FromSeconds(visibilityTimeout + 1)); // Wait until visibilitytimeout of all items (+1) has expired
+            ce1 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            ce3 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            Assert.Equal("1", ce1.Payload);
+            Assert.Equal("3", ce3.Payload);
+        }
+
+        [Fact]
+        public void PublicationDate_Custom()
+        {
+            // Arrange
+            var topicName = "StandardOrderingTests.PublicationDate_Custom";
+            var subName = topicName + "_Sub1";
+            var topic = _publisher.AddOrUpdateTopic(new Topic { Name = topicName });
+            var sub1 = _consumer.AddOrUpdateSubscription(new Subscription
+            {
+                Name = subName, // When ordered not set, delivery should still be ordered on publicationdateutc
+                TopicSubscriptions = new List<TopicSubscription> { new TopicSubscription { TopicId = topic.Id.Value, Enabled = true } },
+            });
+
+            _publisher.Publish(topicName, payload: "1", publicationDateUtc: DateTime.UtcNow.AddSeconds(1));
+            _publisher.Publish(topicName, payload: "2", publicationDateUtc: DateTime.UtcNow);
+            _publisher.Publish(topicName, payload: "3", publicationDateUtc: DateTime.UtcNow.AddSeconds(2));
+
+            var visibilityTimeout = 2;
+            var ce1 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            var ce2 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            var ce3 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            Assert.Equal("2", ce1.Payload);
+            Assert.Equal("1", ce2.Payload);
+            Assert.Equal("3", ce3.Payload);
+            _consumer.MarkConsumed(ce1.Id, ce1.DeliveryKey); // ce1 should be gone
+
+            Thread.Sleep(TimeSpan.FromSeconds(visibilityTimeout + 1)); // Wait until visibilitytimeout of all items (+1) has expired
+            ce2 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            ce3 = _consumer.ConsumeNext(subName, visibilityTimeout: visibilityTimeout).SingleOrDefault();
+            Assert.Equal("1", ce2.Payload);
+            Assert.Equal("3", ce3.Payload);
+        }
+    }
+}
