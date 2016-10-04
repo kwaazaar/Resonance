90b1e728603a30f5c10d32f09d755520ad68dc1b
robertka <robertka@ROBERTKA01>
2016-10-04 16:48:42 +0200

    Added net452 target to test-project to run tests on both .NET Core and .NET Framework 4.52.

--- Resonance.Tests/BaseFixture.cs
+++ Resonance.Tests/BaseFixture.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace Resonance.Tests
 {
@@ -13,8 +14,8 @@ namespace Resonance.Tests
         public BaseFixture()
         {
             var builder = new ConfigurationBuilder()
-                            //.SetBasePath(PlatformServices.Default.Application.ApplicationBasePath)
-                            .SetBasePath(AppContext.BaseDirectory)
+                            .SetBasePath(PlatformServices.Default.Application.ApplicationBasePath)
+                            //.SetBasePath(AppContext.BaseDirectory)
                             .AddJsonFile("xunit.runner.json", optional: false, reloadOnChange: false)
                             .AddEnvironmentVariables();
             var config = builder.Build();
