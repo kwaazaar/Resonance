075c17a59c5f272cebaefe3790476fae9bde3609
robertka <robertka@ROBERTKA01>
2017-03-08 13:30:27 +0100

    0.7.2 - Updated project system to VS 2017

--- Resonance.Core/Properties/AssemblyInfo.cs
+++ Resonance.Core/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Resonance")]
 [assembly: AssemblyDescription("Eventing library, with support for MsSql and MySql. Ideal for implementing CQRS, business events, pub - sub, microservices, etc.")]
-[assembly: AssemblyCopyright("Copyright © 2016 - Robert te Kaat")]
+[assembly: AssemblyCopyright("Copyright © 2017 - Robert te Kaat")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
